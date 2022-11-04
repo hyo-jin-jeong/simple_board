3aa6f0d43c1fcf6a72ce814ea3dac7faa2378440
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-11-04 20:00:32 +0900

    Feat: post update route 구현

--- route/post.js
+++ route/post.js
@@ -1,8 +1,9 @@
+import authentication from '../middleware/authentication.js';
 import express from 'express';
 import postController from '../controller/post.js';
-
 const router = express.Router();
 
 router.post('/', postController.createPost);
 router.get('/', postController.getPosts);
+router.put('/:id', authentication, postController.updatePost);
 export default router;
