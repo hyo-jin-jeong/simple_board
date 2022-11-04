fc1823423b2750bd0458454a5a2e770db7241a8f
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-11-04 20:42:29 +0900

    Test: post update service unit test 구현

--- __test__/post.service.test.js
+++ __test__/post.service.test.js
@@ -1,10 +1,13 @@
+import * as encryptPassword from '../util/encryptPassword.js';
+
 import { BadRequestException } from '../util/exception/badRequest.exception.js';
 import db from '../db/database.js';
 import postService from '../service/post.js';
 
-describe('post create test', () => {
-  let userId, password, title, content;
+describe('post controller test', () => {
+  let id, userId, password, title, content;
   beforeEach(() => {
+    id = 1;
     userId = 1;
     password = 'abcd134';
     title = '프로젝트 진행중';
@@ -12,32 +15,64 @@ describe('post create test', () => {
       '이번 프로젝트는 게시글 생성, 수정, 삭제 구현을 하는 프로젝트이다.';
   });
 
-  it('return 200 성공 할 경우', async () => {
-    const encryptPassword = 'encrypt';
-    const salt = 'salt';
-    db.User.findById = jest.fn(() => {
-      return { id: 1, name: 'jin' };
-    });
-    db.Post.createPost = jest.fn();
+  describe('post create test', () => {
+    it('성공 할 경우', async () => {
+      const hashPassword = 'hashValue';
+      const salt = 'salt';
+      db.User.findById = jest.fn(() => {
+        return { id, name: 'jin' };
+      });
+      db.Post.createPost = jest.fn();
+      encryptPassword.createEncryptPassword = jest.fn(() => hashPassword);
+      encryptPassword.createSalt = jest.fn(() => salt);
 
-    await postService.createPost(userId, password, title, content);
-
-    expect(db.User.findById).toBeCalledTimes(1);
-    expect(db.User.findById).toBeCalledWith(userId);
-    expect(db.Post.createPost).toBeCalledTimes(1);
-    // expect(db.Post.createPost).toBeCalledWith(
-    //   userId,
-    //   encryptPassword,
-    //   salt,
-    //   title,
-    //   content
-    // );
-  });
-  it('return BadRequestError 존재하지 않는 user인 경우', async () => {
-    db.User.findById = jest.fn();
-
-    expect(async () => {
       await postService.createPost(userId, password, title, content);
-    }).rejects.toThrowError(BadRequestException);
+
+      expect(db.User.findById).toBeCalledTimes(1);
+      expect(db.User.findById).toBeCalledWith(userId);
+      expect(db.Post.createPost).toBeCalledTimes(1);
+      expect(db.Post.createPost).toBeCalledWith(
+        userId,
+        hashPassword,
+        salt,
+        title,
+        content
+      );
+    });
+    it('return BadRequestError 존재하지 않는 user인 경우', async () => {
+      db.User.findById = jest.fn();
+
+      expect(async () => {
+        await postService.createPost(userId, password, title, content);
+      }).rejects.toThrowError(BadRequestException);
+    });
+  });
+
+  describe('post update test', () => {
+    it('', async () => {
+      db.Post.findById = jest.fn(() => {
+        return {
+          id,
+          password,
+          title,
+          content,
+        };
+      });
+      db.Post.updatePost = jest.fn();
+
+      await postService.updatePost(id, content, title);
+
+      expect(db.Post.findById).toBeCalledTimes(1);
+      expect(db.Post.findById).toBeCalledWith(id);
+      expect(db.Post.updatePost).toBeCalledTimes(1);
+      expect(db.Post.updatePost).toBeCalledWith(id, content, title);
+    });
+    it('return BadRequestError 존재하지 않는 post인 경우', async () => {
+      db.Post.findById = jest.fn();
+
+      expect(async () => {
+        await postService.updatePost(id, content, title);
+      }).rejects.toThrowError(BadRequestException);
+    });
   });
 });
