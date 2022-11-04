718ff4becbf07c91691127d199fd643873cb4a59
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-11-04 19:59:28 +0900

    Feat: post update service 구현

--- service/post.js
+++ service/post.js
@@ -1,40 +1,15 @@
+import { createEncryptPassword, createSalt } from '../util/encryptPassword.js';
+
 import { BadRequestException } from '../util/exception/badRequest.exception.js';
-import config from '../config.js';
-import crypto from 'crypto';
 import db from '../db/database.js';
-
-const createSalt = () => {
-  return new Promise((resolve, reject) => {
-    crypto.randomBytes(64, (err, buf) => {
-      if (err) reject(err);
-      resolve(buf.toString('base64'));
-    });
-  });
-};
-
-const createEncryptPassword = async plainPassword => {
-  return new Promise(async (resolve, reject) => {
-    const salt = await createSalt();
-    crypto.pbkdf2(
-      plainPassword,
-      salt,
-      config.keyStretching,
-      64,
-      config.algorithm,
-      (err, key) => {
-        if (err) reject(err);
-        resolve({ encryptPassword: key.toString('base64'), salt });
-      }
-    );
-  });
-};
 
 async function createPost(userId, password, title, content) {
   const user = await db.User.findById(userId);
   if (!user) {
     throw new BadRequestException('INVALID VALUE');
   }
-  const { encryptPassword, salt } = await createEncryptPassword(password);
+  const salt = await createSalt();
+  const encryptPassword = await createEncryptPassword(password, salt);
 
   db.Post.createPost(userId, encryptPassword, salt, title, content);
 }
@@ -43,7 +18,16 @@ async function getPosts(id, createAt) {
   return await db.Post.getPosts(id, createAt);
 }
 
+async function updatePost(id, content, title) {
+  const post = await db.Post.findById(id);
+  if (!post) {
+    throw new BadRequestException('INVALID VALUE');
+  }
+  db.Post.updatePost(id, content, title);
+}
+
 export default {
   createPost,
   getPosts,
+  updatePost,
 };
